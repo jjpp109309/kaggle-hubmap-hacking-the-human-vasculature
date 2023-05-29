5b8ed80975514662477c6e0fff483992b3f1e468
J. Jaime Paredes <jjpp109309@gmail.com>
2023-05-29 01:04:34 -0600

    add load_polygons function

--- src/kaggle_vascular_competition/data_loaders.py
+++ src/kaggle_vascular_competition/data_loaders.py
@@ -1,4 +1,6 @@
 import os
+import json;
+
 import numpy as np
 
 from .params import paths 
@@ -9,8 +11,24 @@ def load_image(path: str, *, source: str=None)-> np.ndarray:
     if source is not None:
         path = os.path.join(paths[source], path)
 
-
     image = Image.open(path)
-    image = np.array(image)
+    image = np.array(image, dtype='int')
 
     return image
+
+def load_polygons(image_id: str)-> dict:
+
+    with open(paths['polygons'], 'r') as jsonl_file:
+        for line in jsonl_file:
+            if image_id in line:
+                break
+        else:
+            return []
+
+    data: dict = json.loads(line)
+    
+    polygons: dict = data['annotations']
+    for polygon in polygons:
+        polygon['coordinates'] = np.array(polygon['coordinates'], dtype='int').squeeze()
+
+    return polygons
